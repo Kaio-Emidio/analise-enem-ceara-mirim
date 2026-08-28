738c534ea805e49a7fd2b589162fb8b091fe9938
Kaio Emidio <kaio.emidio@escolar.ifrn.edu.br>
2026-08-27 23:02:48 -0300

    Exercícios da aula 01

---
+++ Aula 01/exercício_aula_01.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('Meu Perfil')
+st.header('Seja bem vindo(a) ao meu site! 🫪')
+st.subheader('Me chamo Kaio!')
+st.markdown('''
+Estou cursando **Informática** 🖥️ e **Jogador de RPG** 🎲
+Sou programador em *Python* e utilizo ele para tudo!
+As áreas que gosto são:
+* Análise de dados
+* Automação
+* Criação de sites
+* Backend
+* E agora estou aprendendo **Streamlit**
+''')
+st.write('Espero que tenha gostado do meu perfil!')
